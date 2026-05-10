342798fb2c707e308c719ad11f31fad3a2e05394
Blake Thompson <157252360+bthom02@users.noreply.github.com>
2026-05-10 15:07:20 +1000

    fixing contrast and aria

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const themeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 const themeToggleBtnEl = document.getElementById("theme-toggle-btn");
-let isDarkTheme = true;
+let isDarkTheme = document.body.getAttribute("data-theme");
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.setAttribute("data-theme", "dark");
